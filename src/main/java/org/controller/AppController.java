8ba1b0dc0d0d97f07b15292e9b844c6f861183a7
javatar <davidschlachter96@gmail.com>
2019-11-18 16:20:52 -0800

    Tweaked Airport.fxml, added some styling (fx-css)

---
+++ src/main/java/org/controller/AppController.java
@@ -0,0 +1,13 @@
+package org.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AppController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
